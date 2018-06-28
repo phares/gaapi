9c2efeef967adf69b275fed5b3dbd935f114b76b
phares <abungaphares@gmail.com>
2018-06-28 15:40:48 +0300

    added ugnicorn to requirements file

--- gaapi/settings.py
+++ gaapi/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_RENDERER_CLASSES': (
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
     ),
 }
